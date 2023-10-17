e19a4e76c203c6629eb367772e0d2eb3f1074e4d
jameschao <jameschao@MacBook-Pro.local>
2023-10-17 14:05:37 +0800

    update script

--- update.bat
+++ update.bat
@@ -1,6 +1,6 @@
 @echo off
-git reset --hard HEAD^
+git reset --hard HEAD~1
 git pull -s recursive -X theirs
-pip install requirements.txt
+pip install -r requirements.txt
 echo "update complete"
 pause
--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git reset --hard HEAD^
+git reset --hard HEAD~1
 git pull -s recursive -X theirs
-pip install requirements.txt
+pip install -r requirements.txt
 echo "update complete"
